cf46d6b6fc3861a150bfa14f918fd21fbc152d01
shadowxh <1343023812@qq.com>
2017-03-29 15:36:35 +0800

    finish history page

--- get_online_races.php
+++ get_online_races.php
@@ -38,9 +38,10 @@
 		return $Data;
 	}
 	
-	function addobj($record)
+	function addobj($record,$flag)
 	{
-		$man=new Racer($record['Driver_Reg'],$record['Car_no'],$record['Driver_name'],$record['Team'],$record['Best_time'],$record['Best_time_DDD'],$record['Last_time'],$record['Last_time_DDD'],$record['Laps'],$record['Total_time'],$record['Total_time_DDD']);
+		$flag=(int)$flag;
+		$man=new Racer($record['Driver_Reg'],$record['Car_no'],$record['Driver_name'],$record['Team'],$record['Best_time'],$record['Best_time_DDD'],$flag==1?"":$record['Last_time'],$flag==1?"":$record['Last_time_DDD'],$record['Laps'],$record['Total_time'],$record['Total_time_DDD']);
 		return $man;
 	}
 	function cmp($a,$b)
@@ -49,7 +50,41 @@
 		$tmp2=($b->total_time).($b->total_time_ddd);
 		return $tmp1>$tmp2?1:-1;
 	}
-	
+	function cal_time($time,$ms)
+	{
+		$t=explode(":",$time);
+		$h=(int)$t[0];$m=(int)$t[1];$s=(int)$t[2];
+		$ret=$h*3600+$m*60+$s;
+		$ret=$ret*1000+(int)$ms;
+		return $ret;
+	}
+	function to_int($double)
+	{
+		return (int)(floor($double));
+	}
+	function cal_diff($time1,$ms1,$time2,$ms2)
+	{
+		$ret="";
+		$cnt1=cal_time($time1,$ms1);$cnt2=cal_time($time2,$ms2);
+		$cnt=$cnt1-$cnt2;
+		$h_unit=60*60*1000;
+		$hh=to_int($cnt/$h_unit);
+		if($hh)
+		{
+			$ret.=(($hh<10?"0":"").(string)$hh.":");
+		}
+		$cnt%=$h_unit;
+		$mm=to_int($cnt/(60*1000));
+		$ret.=(($mm<10?"0":"").(string)$mm.":");
+		$cnt%=(60*1000);
+		$ss=to_int($cnt/1000);
+		$ret.=(($ss<10?"0":"").(string)$ss.".");
+		$ms=$cnt%1000;
+		$ret.=(string)$ms;
+		if($ms<10) $ret.="00";
+		else if($ms<100) $ret.="0";
+		return $ret;
+	}
 	function add_race($one_race)
 	{
 		return '
@@ -97,7 +132,7 @@
 		';
 
 	}
-	function add_man($racers)
+	function add_man($racers,$flag)
 	{
 		$ret='
 			<div class="row-fluid">
@@ -114,11 +149,8 @@
 												<th>排名</th>
 												<th>车号</th>
 												<th>车手</th>
-												<th>车队</th>
-                                                <th>最快单圈</th>
-                                                <th>当前圈速</th>
-                                                <th>总圈数</th>
-												<th>总时间</th>
+												<th>车队</th>'.($flag=="1"?'':'<th>最快单圈</th><th>当前圈速</th><th>总圈数</th>').
+												'<th>总时间</th>
 												<th>差距</th>
 											</tr>
 										</thead>
@@ -129,12 +161,12 @@
 				<td>'.($i+1).'</td>
 				<td>'.$racers[$i]->car_no.'</td>
 				<td>'.$racers[$i]->_name.'</td>
-				<td class="center">'.$racers[$i]->team.'</td>
-				<td class="center">'.$racers[$i]->best_time.'.'.$racers[$i]->best_time_ddd.'</td>
+				<td class="center">'.$racers[$i]->team.'</td>'
+				.($flag=="1"?'':('<td class="center">'.$racers[$i]->best_time.'.'.$racers[$i]->best_time_ddd.'</td>
                                 <td class="center">'.$racers[$i]->last_time.'.'.$racers[$i]->last_time_ddd.'</td>
-                                <td class="center">'.$racers[$i]->laps.'</td>
-                                <td class="center">'.$racers[$i]->total_time.'.'.$racers[$i]->total_time_ddd.'</td>
-				<td class="center">'.($i==0?"":"difference").'</td>
+                                <td class="center">'.$racers[$i]->laps.'</td>')).
+                                '<td class="center">'.$racers[$i]->total_time.'.'.$racers[$i]->total_time_ddd.'</td>
+				<td class="center">'.($i==0?"":cal_diff($racers[$i]->total_time,$racers[$i]->total_time_ddd,$racers[$i-1]->total_time,$racers[$i-1]->total_time_ddd)).'</td>
 				</tr>';
 		}								
 		$ret.='</tbody></table></div></div></div>
@@ -144,7 +176,7 @@
 		return $ret;
 	}
 
-	if(!empty($_GET))
+	if(!empty($_GET) && ($_GET['page']=='0'))
 	{
 		$response="";
 		$races=get_data_from("testcom","now");
@@ -168,7 +200,7 @@
 		
 		foreach($races as $record)
 		{
-			$race_ids_unique[$record['Race_id']][]=addobj($record);
+			$race_ids_unique[$record['Race_id']][]=addobj($record,$_GET['page']);
 		}
 		$Data=get_data_from("testcom","race");
 		$all_races=array();
@@ -187,7 +219,47 @@
 
 
 	}
+	if(!empty($_GET) && ($_GET['page']=='1'))
+	{
+		$response="";
+                $races=get_data_from("testcom","history");
+                $race_ids=array();
+                foreach($races as $record)
+                {
+                        $race_ids[]=$record['Race_id'];
+                }
+                $cnt=count($race_ids);
+                if(!$cnt) {echo "no historical races!";exit(1);}
+                sort($race_ids);
+                $race_ids_unique=array();
+                $race_ids_unique[$race_ids[0]]=array();
+                for($i=1;$i<count($race_ids);$i++)
+                {
+                        if($race_ids[$i]!=$race_ids[$i-1])
+                        {
+                                $race_ids_unique[$race_ids[$i]]=array();
+                        }
+                }
+		foreach($races as $record)
+                {
+                        $race_ids_unique[$record['Race_id']][]=addobj($record,$_GET['page']);
+                }
+                $Data=get_data_from("testcom","race");
+                $all_races=array();
+                foreach($Data as $record)
+                {
+                        $all_races[$record['Race_id']]=array($record['Race'],$record['Race_Group'],$record['Race_Event'],$record['Circuit_Name'],$record['Circuit_Length'],$record['Race_Run'],$record['Race_Type'],$record['Race_start_Data'],$record['Race_start_Time']);
+                }
+                foreach($race_ids_unique as $key=>$val)
+                {
+                        $response.=add_race($all_races[$key]);
+                        usort($race_ids_unique[$key],"cmp");
+                        $response.=add_man($race_ids_unique[$key],$_GET['page']);
+                }
 
+                echo $response;
+
+	}
 
 
 ?>
